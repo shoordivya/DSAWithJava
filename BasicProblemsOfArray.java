6c84733903920188b8fda585aaa5ae83068b8b32
Divya Shoor <shoordivya22@gmail.com>
2026-09-07 23:19:03 +0530

    Reversing an array with and without chnaging orignal array

--- BasicProblemsOfArray.java
+++ BasicProblemsOfArray.java
@@ -8,15 +8,51 @@
 import java.util.Scanner;
 
 public class BasicProblemsOfArray {
-    
+
+    static void reverseOrignalArray(int[] data) {
+        int j = 0, i = data.length - 1;
+        while (i > j) {
+            int temp = data[j];
+            data[j] = data[i];
+            data[i] = temp;
+            i--;
+            j++;
+        }
+        System.out.println("After Reversing Array : ");
+        printArray(data);
+    }
+
+    static void printArray(int[] data) {
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void reverseArray(int[] data) {
+        int size = data.length;
+        int[] ans = new int[size];
+        int j = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            ans[j++] = data[i];
+        }
+        System.out.println("After Reversing Array : ");
+        printArray(ans);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of Array : ");
         int size = sc.nextInt();
-        int[] elements=new int[size];
-        System.out.println("Enter "+size+" elements of array :");
-        for(int i =0;i<size;i++){
-            elements[i]=sc.nextInt();
+        int[] elements = new int[size];
+        System.out.println("Enter " + size + " elements of array :");
+        for (int i = 0; i < size; i++) {
+            elements[i] = sc.nextInt();
         }
+        reverseArray(elements);
+        System.out.println("Orignal Array : ");
+        printArray(elements);
+        reverseOrignalArray(elements);
     }
 }
